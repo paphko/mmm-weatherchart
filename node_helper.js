fca510838859d6620f97955440b3b644fe15c588
MartinGris <martin@grisard.net>
2020-11-23 06:29:24 +0100

    snychronized deletion of old png files (#16)
    * snychronized deletion of old png files
    
    snychronized deletion of old png files
    
    * delete png files only if http get was successful
    
    delete png files only if http get was successful

--- node_helper.js
+++ node_helper.js
@@ -18,9 +18,11 @@ module.exports = NodeHelper.create({
 				host: payload.domain,
 				path: payload.path
 			};
+			
 			http.get(options, function (response) {
 				var pngFiles = payload.mmDir + 'modules/mmm-weatherchart/cache/*.png';
-				del([pngFiles]);
+				del.sync([pngFiles]);
+				
 				var cachedFile = 'modules/mmm-weatherchart/cache/map-' + new Date().getTime() + '.png';
 				var newImage = fs.createWriteStream(payload.mmDir + cachedFile);
 				response.on('data', function(chunk){
